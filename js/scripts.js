9f5f3120b15eb6c252aa6837c01894fd0940c0b8
Joy Vivika <joiemumo@gmail.com>
2018-09-17 09:40:36 +0300

    indentation and spacing

--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,21 @@ var sideA=parseFloat(sides[0]));
 var sideB=parseFloat(sides[1]));
 var sideC=parseFloat(sides[2]));
 
-console.log(typeof (sideA));
+console.log(typeof (sideA))
 console.log(sideB);
-console.log(sideC)
+console.log(sideC);
   // var sideA=parseInt(document.getElementById("sideA").value;
   // var sideB=parseInt(document.getElementById("sideB").value;
   // var sideC=parseInt(document.getElementById("sideC").value;
 
-  if (sideA==0 || sideB==0 || sideC==0) {
-document.getElemtById("sideA").innerHTML= ("This is not a triangle")
 
-  }
-  else if (sideA==sideB==sideC) {
+  if (sideA===sideB===sideC) {
   document.getElemtById("sideB").innerHTML= ("This is an equilateral triangle")
   }
-
-  else if (sideA==sideB!=sideC ||sideA==sideC!=sideB ||sideB==sideC!=sideA) {
+  else if (sideA===0 || sideB===0 || sideC===0) {
+document.getElemtById("sideA").innerHTML= ("This is not a triangle")
+}=
+  else if (sideA===sideB!=sideC ||sideA==sideC!=sideB ||sideB===sideC!=sideA) {
   document.getElemtById("sideB").innerHTML=("This is an isoceles triangle")
   }
   else if (sideA!=sideB!=sideC) {
